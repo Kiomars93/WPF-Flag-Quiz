166a356fe5c6d7dd02526d93948bdfc154ac4ac9
Kio <48633146+Kiomars93@users.noreply.github.com>
2020-10-26 23:31:08 +0100

    Testing Multiple Pages
    Trying to change inbetween the pages. Not done yet!

---
+++ FlagQuiz/FlagQuiz/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlagQuiz
+{
+    public partial class MainWindow : Window
+    {
+        Frame frame;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Start();
+
+            Loaded += MainWindow_Loaded;
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //frame.NavigationService.Navigate(new Uri("Page1.xaml", UriKind.Relative));
+            frame.NavigationService.Navigate(new Page1());
+        }
+
+        private void Start()
+        {
+            // Window options
+            Title = "GUI App";
+            Width = 400;
+            Height = 300;
+            WindowStartupLocation = WindowStartupLocation.CenterScreen;
+
+            // Scrolling
+            ScrollViewer root = new ScrollViewer();
+            root.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+            Content = root;
+
+            // Main grid
+            Grid grid = new Grid();
+            root.Content = grid;
+            grid.Margin = new Thickness(5);
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+
+            frame = new Frame
+            {
+                Content = "Frame in da brain",
+                NavigationUIVisibility = NavigationUIVisibility.Hidden
+            };
+            grid.Children.Add(frame);
+        }
+    }
+}
---
+++ FlagQuiz/FlagQuiz/Page1.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlagQuiz
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            Start();
+        }
+
+        private void Start()
+        {
+            // Window options
+            Title = "GUI App";
+            Width = 400;
+            Height = 300;
+
+            // Scrolling
+            ScrollViewer root = new ScrollViewer();
+            root.VerticalScrollBarVisibility = ScrollBarVisibility.Auto;
+            Content = root;
+
+            // Main grid
+            Grid grid = new Grid();
+            root.Content = grid;
+            grid.Margin = new Thickness(5);
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+
+            var testButton = new Button
+            {
+                Content = "Test Button"
+            };
+            grid.Children.Add(testButton);
+
+            testButton.Click += ButtonClick;
+        }
+        private void ButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Page2());
+        }
+    }
+}
